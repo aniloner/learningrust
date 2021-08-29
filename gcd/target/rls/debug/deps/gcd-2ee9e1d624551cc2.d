dbf3c196418faf2a5e5b2f3b5350c1d25eebc382
anil <aniloner87@gmail.com>
2021-08-30 00:57:54 +0300

    .gitignore is now working

--- gcd/target/rls/debug/deps/gcd-2ee9e1d624551cc2.d
+++
@@ -1,5 +0,0 @@
-/Users/anil/Projects/learningrust/learningrust/gcd/target/rls/debug/deps/gcd-2ee9e1d624551cc2.rmeta: src/main.rs
-
-/Users/anil/Projects/learningrust/learningrust/gcd/target/rls/debug/deps/gcd-2ee9e1d624551cc2.d: src/main.rs
-
-src/main.rs:
